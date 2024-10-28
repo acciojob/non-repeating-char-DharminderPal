d5085d99dd2852a590d05ffa6e2de0b7dfa94972
dharminder <144053986+DharminderPal@users.noreply.github.com>
2024-10-28 14:45:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+return firstNonRepeatedChar('aabbcdd');
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
